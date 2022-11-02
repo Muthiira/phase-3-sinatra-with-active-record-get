ddd4654e9a543a04bd56cf42077693584960c160
Muthiira <calebmuchiri48@gmail.com>
2022-11-02 15:06:40 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < Sinatra::Base
-
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+  get '/games' do
+    # get all games from the database
+    games = Game.all.order(:title).limit(10)
+    # return a json response with an array of all the game data
+    games.to_json
   end
 
+    get '/games/:id' do 
+      # look uo game in the database using id
+      game = Game.find(params[:id])
+      # send a json formatted response of the game data
+      game.to_json(only: [:id, :title, :genre, :price], include: {
+        reviews: { only: [:comment, :score], include: {
+          user: { only: [:name] }
+        } }
+      })  end
 end
